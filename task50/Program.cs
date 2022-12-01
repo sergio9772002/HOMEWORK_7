5cfed220dc60bd3c2884edd9a0bd62b06a76d68c
Sergio <sergio977@rambler.ru>
2022-12-01 15:09:47 +0500

    add task50

---
+++ task50/Program.cs
@@ -0,0 +1,86 @@
+﻿/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет
+[0][1][2][3]
+1 4 7 2
+[4][5][6][7]
+5 9 2 3
+[8][9][10][11]
+8 4 2 4
+
+9 -> 4
+5 -> 9
+*/
+int[,] generate2Darray(int lengthRow, int lengthCol, int deviation)
+{
+    int[,] array = new int[lengthRow, lengthCol];
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+    }
+    return array;
+}
+
+void printColor(string information, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(information);
+    Console.ResetColor();
+}
+void print2DArray(int[,] array)
+{
+    Console.Write("\t");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void elenentOnPos(int[,] array)
+{
+    Console.WriteLine("Введите позицию элемента в массиве");
+    int pos = Convert.ToInt32(Console.ReadLine());
+    int length = array.GetLength(1);
+    int element = 0;
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (pos ==(j + i * length))
+            {
+                element = array[i, j];   
+                count++; 
+            }
+        }
+    }
+    if (count > 0) Console.WriteLine($"на позиции {pos} находится число  {element}");
+    else
+    {
+        Console.WriteLine("такого элемента нет");
+    }
+} 
+int[,] array = generate2Darray(7, 8, 100);
+print2DArray(array);
+elenentOnPos(array);
