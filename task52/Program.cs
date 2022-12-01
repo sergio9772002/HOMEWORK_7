2c76f600d24431861e637e5bc94b7b90d4473cf6
Sergio <sergio977@rambler.ru>
2022-12-01 16:10:59 +0500

    add task52

---
+++ task52/Program.cs
@@ -0,0 +1,66 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+int[,] generate2Darray(int lengthRow, int lengthCol, int deviation)
+{
+    int[,] array = new int[lengthRow, lengthCol];
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+    }
+    return array;
+}
+
+void printColor(string information, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(information);
+    Console.ResetColor();
+}
+void print2DArray(int[,] array)
+{
+    Console.Write("\t");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MidleCol(int[,] array)
+{
+    int j = 0; 
+    while (j < array.GetLength(1))
+    {
+        double midleSum = 0;
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];   
+        }
+        midleSum = sum / array.GetLength(0);
+        Console.Write($"{midleSum}; ");
+        j++;
+    }
+} 
+int[,] array = generate2Darray(5, 5, 100);
+print2DArray(array);
+Console.WriteLine("Среднеарифметическое столбцов равно");
+MidleCol(array);
